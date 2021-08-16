c5486876dae6a7c737d165d9c7caf4f10c92ade5
meishimomura <mei.shimomura@icloud.com>
2021-08-16 17:37:01 +0900

    WebSocket通信処理

--- button.py
+++ button.py
@@ -1,14 +1,31 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
 import RPi.GPIO as GPIO
 import time
+from websocket import create_connection
+
 PNO23 = 23
-#GPIO.cleanup()
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(PNO23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+CONNECTION_URI = os.environ.get("CONNECTION_URI")
+
+ws = create_connection(CONNECTION_URI)
+ws.send(r'{"command": "subscribe","identifier": "{\"channel\": \"HelpButtonChannel\"}"}')
+result =  ws.recv()
+print("First received '%s'" % result)
+
 try:
   while True:
     sw = GPIO.input(PNO23)
     if(sw == GPIO.LOW):
-      print("switch on!")
+      print("Switch on!")
+      ws.send(r'{"command": "message","identifier": "{\"channel\": \"HelpButtonChannel\"}", "data":"{\"action\": \"sendToHelper\", \"message\":\"テスト！\"}"}')
+      print("Sent")
     else:
       print(".")
     time.sleep(0.5)
