e66f36501577d5f3857fa3211f28ab0dc8f3a77d
meishimomura <mei.shimomura@icloud.com>
2021-08-16 23:32:10 +0900

    現在地取得処理を追加

--- button.py
+++ button.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 import RPi.GPIO as GPIO
 import time
 from websocket import create_connection
+import geocoder
 
 PNO23 = 23
 GPIO.setmode(GPIO.BCM)
@@ -14,17 +15,23 @@ load_dotenv(dotenv_path)
 
 CONNECTION_URI = os.environ.get("CONNECTION_URI")
 
+UID = os.environ.get("UID")
+
 ws = create_connection(CONNECTION_URI)
 ws.send(r'{"command": "subscribe","identifier": "{\"channel\": \"HelpButtonChannel\"}"}')
 result =  ws.recv()
-print("First received '%s'" % result)
+print("received '%s'" % result)
 
 try:
   while True:
     sw = GPIO.input(PNO23)
     if(sw == GPIO.LOW):
       print("Switch on!")
-      ws.send(r'{"command": "message","identifier": "{\"channel\": \"HelpButtonChannel\"}", "data":"{\"action\": \"sendToHelper\", \"message\":\"テスト！\"}"}')
+      g = geocoder.ip('me')
+      print("current location: '%s'" % g.latlng)
+      lat = g.lat
+      lng = g.lng
+      ws.send(r'{"command": "message","identifier": "{\"channel\": \"HelpButtonChannel\"}", "data":"{\"action\": \"sendToHelper\", \"status\":\"SUCCESS\", \"message\":\"Sent a help message from Help button\", \"uid\":\"%s\", \"lat\":\"%f\", \"lng\":\"%f\"}"}' % (UID, lat, lng))
       print("Sent")
     else:
       print(".")
